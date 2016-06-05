b367df38698984641c7196b24e22d029f05f0060
Morkalork <maffelu@gmail.com>
2016-06-05 19:05:35 +0200

    MRSPLAY-5: Added a generic ajax function

---
+++ src/js/ajax.js
@@ -0,0 +1,18 @@
+var ajax = {
+  /**
+   * Create an Ajax call
+   * @param url The URL to where you want to make your ajax call
+   * @param callback The action you want to take when the call returns
+   */
+  call(url, callback){
+    var xmlhttp;
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        callback(xmlhttp.responseText);
+      }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
+};
